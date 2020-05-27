b987669c2fa561754e812ce6a1dc49f95bf9a622
Riley Stephens <riley.stephens28@gmail.com>
2020-05-27 16:06:15 -0500

    foundation

---
+++ src/amplitude/amplitude.js
@@ -0,0 +1,30 @@
+import amplitude from 'amplitude-js'
+import DeviceInfo from 'react-native-device-info';
+import DefaultPreference from 'react-native-default-preference';
+
+
+
+export function logEvent(categoryName, eventName, eventData={}) {
+  /**
+  * Summary: Log Event to amplitude
+  *
+  * @param {string}   categoryName    options: App, Registration, Login, Onboarding, Tutorial, External Account, Playlist, Library, Search, Share, Feedback,
+  * @param {string}   eventName       description of action within category
+  */
+
+  categoryName = categoryName.replace(/\b\w/g, l => l.toUpperCase())
+  eventName = eventName.replace(/\b\w/g, l => l.toUpperCase())
+  // console.log(`Firing Event: ${categoryName} - ${eventName} with properties ${JSON.stringify(eventData)}`);
+  amplitude.getInstance().logEventWithTimestamp(`${categoryName} - ${eventName}`, eventData);
+}
+
+export async function setUserData() {
+  var userId = await DefaultPreference.get('user_id')
+  amplitude.getInstance().setUserId(userId)
+}
+
+export async function setRegistration(userId) {
+  setUserData()
+  var identify = new amplitude.Identify().setOnce('Register Date', new Date())
+  amplitude.identify(identify); // Send the Identify call
+}
