a76b475610b62494855b9f527727715d18ae3a8e
Noah Templet <noahtemp13@gmail.com>
2020-06-19 16:02:12 -0500

    Analytics card for charity stuffs

--- src/views/Stats/Dashboard.jsx
+++ src/views/Stats/Dashboard.jsx
@@ -26,6 +26,7 @@ import VectorMap from "components/Map/VectorMap.jsx";
 import BarGraph from "components/Charts/BarGraph.jsx";
 import LineGraph from "components/Charts/LineGraph.jsx";
 import GraphComp from "components/Charts/GraphComp.jsx";
+import CharityCard from "components/Cards/CharityAnalytics.jsx";
 
 // reactstrap components
 import {
@@ -48,76 +49,79 @@ class Dashboard extends React.Component {
   render() {
     return (
       <div className="content">
-      <Container style={{ maxWidth: "90%" }}>
-        <Row>
-          <Col xs="12">
-            <h1 className="m-0" style={{ fontSize: "3rem" }}>Welcome, {this.props.user.displayName}</h1>
-            {/*<Card className="card-chart">
-              <CardHeader className="m-auto mr-auto">
-                  <CardTitle tag="h4">The following charts/graphs are displaying stock data. User generated data is coming soon. </CardTitle>
-              </CardHeader>
-            </Card>*/}
-          </Col>
-          {/* Stream/Listener Cols */}
-          <Col lg="3" md="6" sm="6" xs="12">
-            <GraphComp
-              type="card"
-              data_type="num_streams"
-              icon="icon-headphones"
-              pills={[ "1w", "3m", "6m" ]}
+        <Container style={{ maxWidth: "90%" }}>
+          <Row>
+            <Col xs="12" sm="9" md="9" className="d-flex">
+              <h1 className="m-0 mb-0 mt-auto" style={{ fontSize: "3.25rem" }}>Welcome, {this.props.user.displayName}</h1>
+              {/*<Card className="card-chart">
+                <CardHeader className="m-auto mr-auto">
+                    <CardTitle tag="h4">The following charts/graphs are displaying stock data. User generated data is coming soon. </CardTitle>
+                </CardHeader>
+              </Card>*/}
+            </Col>
+            <Col xs="12" sm="3" md="3">
+              <CharityCard />
+            </Col>
+            {/* Stream/Listener Cols */}
+            <Col lg="3" md="6" sm="6" xs="12">
+              <GraphComp
+                type="card"
+                data_type="num_streams"
+                icon="icon-headphones"
+                pills={[ "1w", "3m", "6m" ]}
+              />
+            </Col>
+            <Col lg="3" md="6" sm="6" xs="12">
+              <GraphComp
+                type="card"
+                data_type="listeners"
+                pills={[ "1w", "3m", "6m" ]}
+              />
+            </Col>
+            <Col lg="3" md="6" sm="6" xs="12">
+              <GraphComp
+                type="card"
+                data_type="num_streams"
+                period="all-time"
+                icon="icon-headphones"
+              />
+            </Col>
+            <Col lg="3" md="6" sm="6" xs="12">
+              <GraphComp
+                type="card"
+                data_type="listeners"
+                period="all-time"
+              />
+            </Col>
+            {/* Line Graph Col */}
+            <Col xs="12">
+              <GraphComp
+                type="line"
+                data_type="num_streams"
+                pills={[ "1w", "3m", "6m", "All-Time" ]}
+                defaultPill={3}
+              />
+            </Col>
+            <Col lg="6">
+              <GraphComp
+                type="bar"
+                data_type="num_streams"
+                pills={[ "1w", "3m", "6m", "All-Time" ]}
+                defaultPill={3}
             />
-          </Col>
-          <Col lg="3" md="6" sm="6" xs="12">
-            <GraphComp
-              type="card"
-              data_type="listeners"
-              pills={[ "1w", "3m", "6m" ]}
-            />
-          </Col>
-          <Col lg="3" md="6" sm="6" xs="12">
-            <GraphComp
-              type="card"
-              data_type="num_streams"
-              period="all-time"
-              icon="icon-headphones"
-            />
-          </Col>
-          <Col lg="3" md="6" sm="6" xs="12">
-            <GraphComp
-              type="card"
-              data_type="listeners"
-              period="all-time"
-            />
-          </Col>
-          {/* Line Graph Col */}
-          <Col xs="12">
-            <GraphComp
-              type="line"
-              data_type="num_streams"
-              pills={[ "1w", "3m", "6m", "All-Time" ]}
-              defaultPill={3}
-            />
-          </Col>
-          <Col lg="6">
-            <GraphComp
-              type="bar"
-              data_type="num_streams"
-              pills={[ "1w", "3m", "6m", "All-Time" ]}
-              defaultPill={3}
-          />
-          </Col>
-          <Col lg="6">
-            <GraphComp
-              type="bar"
-              data_type="listeners"
-              pills={[ "1w", "3m", "6m", "All-Time" ]}
-              defaultPill={3}
-            />
-          </Col>
-          {/*<Col lg="12">
-            <VectorMap />
-          </Col>*/}
-        </Row>
+            </Col>
+            <Col lg="6">
+              <GraphComp
+                type="bar"
+                data_type="listeners"
+                pills={[ "1w", "3m", "6m", "All-Time" ]}
+                defaultPill={3}
+              />
+            </Col>
+            {/*<Col lg="12">
+              <VectorMap />
+            </Col>*/}
+          </Row>
         </Container>
       </div>
     );
